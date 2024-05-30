d4bd18cc5432f1381d6e52441aeb2d583e49cfdd
chokinyan <sabou2344@gmail.com>
2024-05-30 07:34:01 +0200

    vs code + jquery new version

--- index.js
+++ index.js
@@ -5,17 +5,22 @@
     const app = express();
     const port = process.env.PORT || 3000;
 
-    app.use('/',express.static(path.join(__dirname,'solar_concept/index.html')));
+//--------------------------------------------------------------------------------------------------------------
 
+    app.use('/',express.static(path.join(__dirname,'solar_concept')));
 
-    app.get("/", (req, res) =>{
-        console.log(req);
-        res.sendFile(path.join(__dirname,'solar_concept/index.html'));
-        //res.send('./solar concept');
+    app.get('/', (req, res) =>{
+        res.sendFile(path.join(__dirname,'solar_concept'));
     });
 
+    app.get('/test',(req, res) =>{
+        console.log(req);
+        res.send("test ok");
+    });
 
-    app.listen(()=>{
+//--------------------------------------------------------------------------------------------------------------
+
+    app.listen(port,()=>{
         console.log(`Server is running on port ${port}`);
     });
 
---
+++ solar_concept/asset/js/index.js
@@ -0,0 +1,12 @@
+async function charger (){
+    console.log("page charger");
+    $(function (){
+        if(navigator.userAgent.toLowerCase().includes("mobile")){
+            $('p:first').text("Mobile");
+        }
+        else{
+            $('p:first').text("Desktop");
+        }
+    });
+    console.log(navigator.userAgent);
+};
