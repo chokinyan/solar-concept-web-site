9f6bef8ea60a8579507b4e2418f4a4c5f77c7f65
chokinyan <55279869+chokinyan@users.noreply.github.com>
2024-06-05 13:42:12 +0200

    all directory

--- index.js
+++ index.js
@@ -14,9 +14,48 @@
     });
 
     app.get('/vehicule/film%20protection',(req, res) =>{
-        res.sendFile(path.join(__dirname,"solar_concept/vehicule/index.html"));
+        res.sendFile(path.join(__dirname,"solar_concept/vehicule/film_de_protection/index.html"));
     });
 
+    app.get('/vehicule/covering',(req, res) =>{
+        res.sendFile(path.join(__dirname,"solar_concept/vehicule/covering/index.html"));
+    });
+
+    app.get('/vehicule/teintage%20de%20vitre',(req, res) =>{
+        res.sendFile(path.join(__dirname,"solar_concept/vehicule/teintage_de_vitre/index.html"));
+    });
+
+    app.get('/vehicule/ciel%20etoile',(req, res) =>{
+        res.sendFile(path.join(__dirname,"solar_concept/vehicule/ciel_etoile/index.html"));
+    });
+
+    app.get('/vehicule/flocage',(req, res) =>{
+        res.sendFile(path.join(__dirname,"solar_concept/vehicule/flocage/index.html"));
+    });
+
+    app.get('/vetement/bradage',(req, res) =>{
+        res.sendFile(path.join(__dirname,"solar_concept/vetement/bradage/index.html"));
+    });
+
+    app.get('/vetement/flocage%20textile',(req, res) =>{
+        res.sendFile(path.join(__dirname,"solar_concept/vetement/flocage_textile/index.html"));
+    });
+
+    app.get('/batiment/vitrine%20solaire',(req, res) =>{
+        res.sendFile(path.join(__dirname,"solar_concept/batiment/vitre_solaire/index.html"));
+    });
+
+    /*
+    /vehicule/covering
+    /vehicule/teintage%20de%20vitre
+    /vehicule/ciel%20etoile
+    /vehicule/flocage
+
+    /vetement/bradage
+    /vetement/flocage%20textile
+
+    /batiment/vitrine%20solaire
+    */
 //--------------------------------------------------------------------------------------------------------------
 
     app.listen(port,()=>{
--- solar_concept/asset/js/index.js
+++ solar_concept/asset/js/index.js
@@ -8,31 +8,3 @@ function CheckDevice (){
         }
     });
 };
-
-function MenuBar(){
-    $(function (){
-        const menuList = {
-            "Menu_bar_vehicule" : ".Menu_bar_vehicule_div",
-            "Menu_bar_vetement" : ".Menu_bar_vetement_div",
-            "Menu_bar_batiment" : ".Menu_bar_batiment_div"
-        };
-        var retract = "";
-        $("body").on("click",function(item){
-            let target = item.target.id;
-            if(retract != "" && target != retract){
-                $(`${menuList[retract]}`).css("display", "none");
-                retract = "";
-            }
-            if($(menuList[target]) !=undefined ){
-                if($(`${menuList[target]}`).css("display") == "none"){
-                    $(`${menuList[target]}`).css("display","inline-block");
-                    retract = `${target}`;
-                }
-                else{
-                    $(`${menuList[target]}`).css("display","none");
-                    retract = "";
-                }
-            }
-        });
-    });
-};
