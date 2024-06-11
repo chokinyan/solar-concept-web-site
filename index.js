cdef8cb3a25b923e9583bc160a497487530be4e9
chokinyan <55279869+chokinyan@users.noreply.github.com>
2024-06-11 15:13:19 +0200

    album + video carusel

--- index.js
+++ index.js
@@ -13,46 +13,21 @@
         res.sendFile(path.join(__dirname,'solar_concept'));
     });
 
-    app.get('/vehicule/film%20de%20protection',(req, res) =>{
-        res.sendFile(path.join(__dirname,"solar_concept/vehicule/film_de_protection/index.html"));
+    app.get('/nos%20realisations',(req, res) =>{
+        res.sendFile(path.join(__dirname,'solar_concept/nos_realisations/index.html'));
     });
 
-    app.get('/vehicule/covering',(req, res) =>{
-        res.sendFile(path.join(__dirname,"solar_concept/vehicule/covering/index.html"));
+    app.get('/nos%20realisations/aventador',(req, res) =>{
+        res.sendFile(path.join(__dirname,'solar_concept/nos_realisations/aventador/index.html'));
     });
 
-    app.get('/vehicule/teintage%20de%20vitre',(req, res) =>{
-        res.sendFile(path.join(__dirname,"solar_concept/vehicule/teintage_de_vitre/index.html"));
-    });
 
-    app.get('/vehicule/ciel%20etoile',(req, res) =>{
-        res.sendFile(path.join(__dirname,"solar_concept/vehicule/ciel_etoile/index.html"));
-    });
-
-    app.get('/vehicule/flocage',(req, res) =>{
-        res.sendFile(path.join(__dirname,"solar_concept/vehicule/flocage/index.html"));
-    });
-
-    app.get('/vetement/bradage',(req, res) =>{
-        res.sendFile(path.join(__dirname,"solar_concept/vetement/bradage/index.html"));
-    });
-
-    app.get('/vetement/flocage%20textile',(req, res) =>{
-        res.sendFile(path.join(__dirname,"solar_concept/vetement/flocage_textile/index.html"));
-    });
-
-    app.get('/batiment/vitrine%20solaire',(req, res) =>{
-        res.sendFile(path.join(__dirname,"solar_concept/batiment/vitre_solaire/index.html"));
-    });
 
     /*
     /vehicule/covering
     /vehicule/teintage%20de%20vitre
     /vehicule/ciel%20etoile
     /vehicule/flocage
-
-    /vetement/bradage
-    /vetement/flocage%20textile
 
     /batiment/vitrine%20solaire
     */
